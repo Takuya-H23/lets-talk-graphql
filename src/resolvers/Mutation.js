6d3af732436f3bb52f69f9994dc3c5c850477541
Takuya Hirata <takuya.hirata@dcmail.ca>
2020-04-17 14:35:21 -0400

    comment mutation

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -17,12 +17,12 @@ const Mutation = {
   },
 
   updatePost(parent, { key, input }, { db }) {
+    checkStringLength(key, input.text)
+
     const post = db.posts.findIndex(
       (post) => post.id === input.id && post.key === key
     )
     if (post === -1) throw new Error('Post not found')
-
-    checkStringLength(key, input.text)
 
     const newPost = {
       ...db.posts[post],
@@ -35,14 +35,55 @@ const Mutation = {
   },
 
   deletePost(parent, { key, input }, { db }) {
-    checkStringLength(key)
-
     const post = db.posts.findIndex(
       (post) => post.id === input.id && post.key === key
     )
     if (post === -1) throw new Error('Post not found')
 
+    db.comments = db.comments.filter((comment) => comment.postId !== input.id)
+
     return db.posts.splice(post, 1)[0]
+  },
+
+  // Comment Mutation======================================================
+  createComment(parent, { key, input }, { db }) {
+    const post = db.posts.findIndex((post) => post.id === input.postId)
+    if (post === -1) throw new Error('Post not found')
+
+    checkStringLength(key, input.text)
+
+    const newComment = {
+      ...input,
+      key,
+      id: 'newComment',
+      updatedAt: null,
+      createdAt: new Date(),
+    }
+
+    db.comments.push(newComment)
+    return newComment
+  },
+
+  updateComment(parent, { key, input }, { db }) {
+    checkStringLength(key, input.text)
+
+    const comment = db.comments.find(
+      (comment) => comment.id === input.id && comment.key === key
+    )
+    if (!comment) throw new Error('Comment not found')
+
+    comment.text = input.text
+    comment.updatedAt = new Date()
+    return comment
+  },
+
+  deleteComment(parent, { key, input }, { db }) {
+    const commentIndex = db.comments.findIndex(
+      (comment) => comment.id === input.id && comment.key === key
+    )
+    if (commentIndex === -1) throw new Error('Comment not found')
+
+    return db.comments.splice(commentIndex, 1)[0]
   },
 }
 
