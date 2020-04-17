2d96cd7bd0bbfe4e772b9b035d571af8e5cf3d66
Takuya Hirata <takuya.hirata@dcmail.ca>
2020-04-17 16:33:02 -0400

    fix gitignore

--- src/db/index.js
+++ src/db/index.js
@@ -3,10 +3,7 @@ let posts = [
     id: 'post1',
     key: 'post1',
     text: 'GraphQL',
-<<<<<<< HEAD
     name: 'Andrew',
-=======
->>>>>>> 6d3af732436f3bb52f69f9994dc3c5c850477541
     createdAt: 'Thu Apr 16 2020 21:58:30 GMT-0400',
     updatedAt: null,
   },
@@ -14,10 +11,7 @@ let posts = [
     id: 'post2',
     key: 'post2',
     text: 'Node.js',
-<<<<<<< HEAD
     name: 'Andrew',
-=======
->>>>>>> 6d3af732436f3bb52f69f9994dc3c5c850477541
     createdAt: 'Thu Apr 16 2020 21:58:30 GMT-0400',
     updatedAt: null,
   },
@@ -25,10 +19,7 @@ let posts = [
     id: 'post3',
     key: 'post3',
     text: 'React',
-<<<<<<< HEAD
     name: 'Andrew',
-=======
->>>>>>> 6d3af732436f3bb52f69f9994dc3c5c850477541
     createdAt: 'Thu Apr 16 2020 21:58:30 GMT-0400',
     updatedAt: null,
   },
@@ -39,10 +30,7 @@ let comments = [
     id: 'comment1',
     key: 'comment1',
     text: 'About GraphQL',
-<<<<<<< HEAD
     name: 'Andrew',
-=======
->>>>>>> 6d3af732436f3bb52f69f9994dc3c5c850477541
     postId: 'post1',
     createdAt: 'Thu Apr 16 2020 21:58:30 GMT-0400',
     updatedAt: null,
@@ -51,10 +39,7 @@ let comments = [
     id: 'comment2',
     key: 'comment2',
     text: 'About Node.js',
-<<<<<<< HEAD
     name: 'Andrew',
-=======
->>>>>>> 6d3af732436f3bb52f69f9994dc3c5c850477541
     postId: 'post2',
     createdAt: 'Thu Apr 16 2020 21:58:30 GMT-0400',
     updatedAt: null,
@@ -63,10 +48,7 @@ let comments = [
     id: 'comment3',
     key: 'comment3',
     text: 'About React',
-<<<<<<< HEAD
     name: 'Andrew',
-=======
->>>>>>> 6d3af732436f3bb52f69f9994dc3c5c850477541
     postId: 'post3',
     createdAt: 'Thu Apr 16 2020 21:58:30 GMT-0400',
     updatedAt: null,
